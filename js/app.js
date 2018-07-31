44566bcf123aaece11742d4a6abd99b785d6d42b
Ray zhang <jy02087898@126.com>
2018-07-31 23:22:21 +0800

    add readme and lightbox and settime

--- js/app.js
+++ js/app.js
@@ -2,15 +2,13 @@ let cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-pap
 
 const stars = document.querySelector('.stars');
 
-const deck = document.getElementsByClassName("deck");
+const deck = document.getElementsByClassName("deck")[0];
 
 const card = document.getElementsByClassName('card');
 
-let endcontent = document.createElement('p');
+const box = document.querySelector('.lightbox');
 
-const ul = document.querySelector('.deck');
-
-const sec = document.querySelector('section');
+const content = document.querySelector('.content');
 
 let starsNumber = 0;
 
@@ -19,40 +17,38 @@ let moves = 0;
 let match = 0;
 
 //move counter
-
 function addMoves() {
     moves++;
     document.querySelector('.moves').innerHTML = moves;
 }
 
 // star rating function
-
 function rating() {
-    if (moves === 10) {
+    if (moves  <= 12) {
         stars.innerHTML = '<li><i class="fa fa-star"></i></li> ' + '<li><i class="fa fa-star"></i></li>';
         starsNumber = 2;
-    } else if (moves === 15) {
+    } else if (moves >= 12) {
         stars.innerHTML = '<li><i class="fa fa-star"></i></li> ';
         starsNumber = 1;
-    } else if (moves < 9) {
+    } else if (moves <= 9) {
         stars.innerHTML = '<li><i class="fa fa-star"></i></li> ' + '<li><i class="fa fa-star"></i></li> ' + '<li><i class="fa fa-star"></i></li>';
         starsNumber = 3;
     }
 }
 
 //MATCH num
-
 function matchCounter() {
     match++;
 }
 
 //结束画面
-
 function end() {
     if (match === 8) {
-        ul.style.display = 'none';
-        endcontent.innerHTML = 'CONGRADULATIONS! YOU WON!' + '\nwith ' + moves + ' Moves and ' + starsNumber + ' Stars ';
-        sec.appendChild(endcontent);
+        stopCount();
+        box.style.display = 'block';
+        content.innerHTML ='with ' + moves + ' Moves and ' + starsNumber + ' Stars and ' + c + ' second';
+    }else if (moves === 1) {
+        timedCount();
     }
 }
 /*
@@ -90,8 +86,8 @@ function shuffle(array) {
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
 
-// shuffle
 
+// shuffle
 function shuffleCard() {
     cards = shuffle(cards);
 }
@@ -99,17 +95,17 @@ function shuffleCard() {
 let opened = [];
 
 //监听点击
-
-document.querySelectorAll('li.card').forEach(function (card) {
-        card.addEventListener('click', function() {
-        opened.push(card);
-        card.classList.add('open', 'show', 'disable');
-        checkcard();
-        })
+deck.addEventListener("click", function (e) {
+        let event = e || window.event;
+        let target = event.target || event.srcElement;
+        if (target.nodeName.toLocaleLowerCase() === "li") {
+            opened.push(target);
+            target.classList.add('open', 'show', 'disable');
+            checkcard();
+        }
 })
 
 //检查卡片
-
 function checkcard() {
         if (opened.length === 2) {
             let firstCard = opened[0];
@@ -135,21 +131,46 @@ function checkcard() {
         }
 }
 
-//RESTART
+//计时器
+var c = 0
+var t
 
-document.querySelector('.restart').addEventListener('click', function restart() {
+function timedCount() {
+    c = c + 1
+    t = setTimeout("timedCount()", 1000)
+}
+
+function stopCount() {
+    clearTimeout(t);
+}
+
+function restart() {
     shuffleCard();
     match = 0;
     moves = 0;
     starsNumber = 0;
-    ul.style.display = 'flex';
-    endcontent.innerHTML= "";
+    c = 0;
+    box.style.display = 'none';
+    content.innerHTML = "";
     document.querySelector('.moves').innerHTML = 0;
     document.querySelector('.stars').innerHTML = '<li><i class="fa fa-star"></i></li> ' + '<li><i class="fa fa-star"></i></li> ' + '<li><i class="fa fa-star"></i></li>';
-    document.querySelectorAll('li.card').forEach(function (card,index) {
+    document.querySelectorAll('li.card').forEach(function (card, index) {
         card.classList.remove('match', 'open', 'show', 'disable');
         card.querySelector('i').className = cards[index];
     })
+}
+//RESTART
+document.querySelector('.close').addEventListener('click', function() {
+    box.style.display = 'none';
 })
 
+
+
+document.querySelector('.restart').addEventListener('click', function() {
+    restart();
+})
+
+document.querySelector('.gameStar').addEventListener('click', function() {
+    restart();
+})
 //github https://github.com/Rayjy02087898/memory-game_zh.git
